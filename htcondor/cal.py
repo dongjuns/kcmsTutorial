cacc73305bd5b69e29b567e164b98034c38bfab8
Geonmo Ryu <geonmo@kisti.re.kr>
2018-02-27 21:28:27 +0900

    Add remap.

--- htcondor/cal.py
+++ htcondor/cal.py
@@ -5,5 +5,11 @@ import os, sys
 if ( len( sys.argv) != 2 ) :
     print "wrong argument."
 
+sum =0
 for x in range(10*(int(sys.argv[1])+1))[-10:] :
     print x
+    sum = sum+x
+
+ofile = open("out.txt","w")
+ofile.write("Sum is %d\n"%(sum))
+ofile.close()
